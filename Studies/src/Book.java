18048f6f4c2bb320c72c9b40e726ea34563a8946
Rafael <silva.reis.rafael@gmail.com>
2021-09-09 15:42:34 -0500

    first comit

---
+++ Studies/src/Book.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+class Book {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     Scanner entrada = new Scanner(System.in);
+     
+     System.out.println("Enter the number");
+     int number = entrada.nextInt();
+     
+     System.out.println("Enter the number");
+     int number2 = entrada.nextInt();
+     
+     System.out.println(number+number2);
+	}
+
+}
